a8f7840812870d2782e38026c7c705ea20f283eb
Rushikesh Naik <34379281+rushikeshnaik779@users.noreply.github.com>
2021-08-13 18:49:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,110 @@
+import streamlit as st
+
+
+st.write("""# Rushikesh Naik
+**Data Science Aspirant | Software Engineer | Data Analyst**
+
+
+""")
+st.write("""
+""")
+col1, col2 = st.columns(2)
+
+with col1:
+    
+    st.write("""- **Summary** : 
+    Experienced Software Engineer, looking to use my analytics and programming culture in the world of data. Eager to use my Machine Learning knowledge and statistical analytical skill to find various patterns, insights & decision points from the data and build a predictive model which will help company’s growth and ease the work of human kind.
+    """)
+with col2 : 
+    st.write("""
+    - **Address** : Lahari Chowk, 
+    Dongaon, 443303 India
+- **Mob** : 9545442394 
+- **Email** : rushikeshnaik779@gmail.com
+    """)
+st.write("""
+### FIle Monitoring :  
+ - Automated the file monitoring task with the help of Python script. In this project we developed a script which runs on the scheduled time and detects new files that arrived and the files that are yet to arrive & according to which it creates alerts using SMTP protocol where SME gets the email alert.
+
+- Application Data update Request : Automated the process of updating User and Customer details in App with the help of powershell script. We developed a script which takes the input in GUI of powershell(poshGUI) and updates the user and customer details in MS SQL Database and provide alert after updation or failure. 
+
+- Automation Selenium:Test automation, where we had to do an end to end testing.i.e. from the link to report generation. 
+    - Purpose: test the module’s visibility & loading time. It saved almost 2 hours of the SME daily.  
+### Power BI:
+- Worked on various datasets to visualise the data. Untill now I have delivered  more than 15 dashboards, where I faced different challenges, according to the specific domain. Automated most of the dashboards, that eliminated complete manual intervention.
+""")
+
+st.write("""
+## Skills / Education :
+
+""")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    
+    st.write("""
+    ### Key Skills :
+- Data Analysis
+- Data Visualization 
+- Statistical Methods 
+- Modeling Techniques 
+- Predictive Modelling 
+- Process Improvement 
+- Software Development/Engineering 
+- Automation 
+- Model Deployment
+""")
+with col2 : 
+    st.write("""
+    ### Technical Skills :
+- Python 
+- Power BI 
+- SQL 
+- PowerShell 
+- Java
+- Angular 
+- Pytest
+
+""")
+with col3 : 
+    st.write("""
+    ### Algorithms :
+- Linear Regression
+- Logistic Regression
+- KNN
+- Naive Bayes
+- Decision Tree
+- Random Forest
+- Bagging & Boosting (Ensemble
+Models) 
+- TFIDF
+- BOW
+- Word2vec - RNN
+
+
+""")
+
+st.write(
+"""
+## Personal Projects : 
+- Salary Prediction : To predict the salary of employee based on the information provided in the dataset. It helped company as well as employees to get benefit and optimise.
+
+- Car Price Prediction : Developed and deployed the model using flask.It was a regression problem and in that problem my main focus was to understand Linear Regression with help of dataset present on kaggle. Initial steps were data preprocessing, feature engineering & dealing with missing values & after that I used various models. 
+
+- Sentiment Analysis ;  Covid sentiment detection with help of RNN and LSTM. Developed a model which got 85% F1 score.( Present in GitHub)   
+
+- ChatBot: Chatbot from scratch:Developed a prototype which can understand the need of humans and get the details or provide the details with proper intent. 
+
+- Stock price prediction with tweets: Currently I am working on it. 
+
+"""
+
+)
+
+st.write("""
+### Connect Me :
+- Linkedin : https://www.linkedin.com/in/rushikesh-naik-1543a3169/
+ - Github : https://github.com/rushikeshnaik779
+  - Kaggle : https://www.kaggle.com/rushinaik
+""")
